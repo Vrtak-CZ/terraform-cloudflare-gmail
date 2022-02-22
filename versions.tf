5b7ce0d7a5c92bc60ef624fe32fdc4ef3c233917
Patrik Votoček <patrik@votocek.cz>
2022-02-22 15:09:36 +0100

    update versions

--- versions.tf
+++ versions.tf
@@ -2,6 +2,9 @@ terraform {
   required_version = ">= 0.13.7"
 
   required_providers {
-    cloudflare = ">= 3.9.0"
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = ">= 3.9.0"
+    }
   }
 }
